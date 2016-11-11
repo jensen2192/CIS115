fa86d6b231c8e0caa256f2b5ab30500067dbdc7e
Michael J. Jensen <X00271087@wcc.dom>
2016-11-11 10:38:57 -0600

    larger initial

---
+++ larger.py
@@ -0,0 +1,12 @@
+def main():
+    list = [1, 6, 8, 15, 23, 64, 999, 999999999]
+    print("Displaying numbers in list greater than 'n'")
+    n = int(input("Provide a value for 'n': "))
+    larger_than(n, list)
+
+def larger_than(the_num, the_list):
+    for value in the_list:
+        if value > the_num:
+            print(value)
+
+main()
